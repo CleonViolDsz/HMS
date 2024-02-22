43b33568c6f170a34299be7da1b54336dd32a160
Cleon Dsouza <dsouzacleon90@gmail.com>
2024-02-22 13:42:31 +0530

    HMS

---
+++ assets/DoctorData.js
@@ -0,0 +1,313 @@
+const Emergency = [
+  
+    {
+      doctorId: 1,
+      Name: "John Doe",
+      deptId: 1,
+      Age: "50",
+      Phone: "122445657",
+      Sex: "Male"
+    },
+    {
+      doctorId: 2,
+      Name: "Jennifer lewis",
+      deptId: 1,
+      Age: "45",
+      Phone: "122445657",
+      Sex: "Female"
+    },
+  
+  
+    {
+    doctorId: 3,
+    Name: "sushmitha loknath",
+    deptId: 1,
+    Age: "60",
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+  doctorId: 4,
+  Name: "Chris Brown",
+  deptId: 1,
+  Age: "35",
+  Phone: "122445657",
+  Sex: "Male"
+  },
+  
+  
+  {
+  doctorId: 5,
+  Name: "Taylor Swift",
+  deptId: 1,
+  Age: "40",
+  Phone: "122445657",
+  Sex: "Female"
+  },
+  {
+  doctorId: 6,
+  Name: "Selena Gomez",
+  deptId: 1,
+  Age: "36",
+  Phone: "122445657",
+  Sex: "Female"
+  },
+  
+  {
+    doctorId: 7,
+    Name: "Drake",
+    deptId: 1,
+    Age: "41",
+    Phone: "122445657",
+    Sex: "Male"
+    },
+    
+    {
+      doctorId: 8,
+      Name: "Charlie Puth",
+      deptId: 1,
+      Age: "39",
+      Phone: "122445657",
+      Sex: "Male"
+      },
+  ]
+
+
+const Surgery = [
+  {
+    doctorId: 1,
+    Name: "Michelle obama",
+    deptId: 2,
+    Age: "39",
+    Phone: "122445657",
+    Sex: "Female"
+    },
+
+    {
+      doctorId: 2,
+      Name: "Peter Griffin",
+      deptId: 2,
+      Age: "40",
+      Phone: "122445657",
+      Sex: "Male"
+      },
+]
+
+const ICU = [
+  {
+    doctorId: 1,
+    Name: 'Meagan Janice',
+    deptId: 3,
+    Age: 40,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+    doctorId: 2,
+    Name: 'Silvia',
+    deptId: 3,
+    Age: 49,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+    doctorId: 3,
+    Name: 'Silvestor',
+    deptId: 3,
+    Age: 42,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+]
+
+const ENT = [
+  {
+    doctorId: 1,
+    Name: 'Lois Griffin',
+    deptId: 4,
+    Age: 45,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+    doctorId: 2,
+    Name: 'Zayn Malik',
+    deptId: 4,
+    Age: 62,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 3,
+    Name: 'Scarlet Johanson',
+    deptId: 4,
+    Age: 47,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+]
+
+const Neurology = [
+  {
+    doctorId: 1,
+    Name: 'Meg Griffin',
+    deptId: 5,
+    Age: 40,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+    doctorId: 2,
+    Name: 'Quagmire',
+    deptId: 5,
+    Age: 51,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 3,
+    Name: 'JOhn Timberlake',
+    deptId: 5,
+    Age: 43,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+ 
+]
+
+const Oncology = [
+  
+    {
+      doctorId: 1,
+      Name: 'Gahana Kumari',
+      deptId: 6,
+      Age: 25,
+      Phone: "122445657",
+      Sex: "Female"
+    },
+    {
+      doctorId: 2,
+      Name: 'Kristen',
+      deptId: 6,
+      Age: 52,
+      Phone: "122445657",
+      Sex: "Male"
+    },
+    {
+      doctorId: 3,
+      Name: 'Sheehan',
+      deptId: 6,
+      Age: 70,
+      Phone: "122445657",
+      Sex: "Male"
+    },
+  
+]
+
+const Gynaecology = [
+  {
+    doctorId: 1,
+    Name: 'Amar Shetty',
+    deptId: 7,
+    Age: 69,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 2,
+    Name: 'Christoper',
+    deptId: 7,
+    Age: 55,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 3,
+    Name: 'Sheela',
+    deptId: 7,
+    Age: 38,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+]
+
+const Pediatrics = [
+  {
+    doctorId: 1,
+    Name: 'Shrihari Acharya',
+    deptId: 8,
+    Age: 59,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 2,
+    Name: 'Dinesh Kumar',
+    deptId: 8,
+    Age: 39,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 3,
+    Name: 'Sharmila',
+    deptId: 8,
+    Age: 40,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+]
+
+const Psychiatry = [
+  {
+    doctorId: 1,
+    Name: 'Francis',
+    deptId: 9,
+    Age: 28,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 2,
+    Name: 'Sara Sampaio',
+    deptId: 9,
+    Age: 30,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+    doctorId: 3,
+    Name: 'Irina Shyak',
+    deptId: 9,
+    Age: 40,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+]
+
+const Orthopaedic = [
+  {
+    doctorId: 1,
+    Name: 'Roopesh Acharya',
+    deptId: 10,
+    Age: 25,
+    Phone: "122445657",
+    Sex: "Male"
+  },
+  {
+    doctorId: 2,
+    Name: 'Sankalpa Gowri',
+    deptId: 10,
+    Age: 30,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+  {
+    doctorId: 3,
+    Name: 'Zeena Dsouza',
+    deptId: 10,
+    Age: 25,
+    Phone: "122445657",
+    Sex: "Female"
+  },
+]
---
+++ script.js
@@ -0,0 +1,182 @@
+//drop down menu
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  }); 
+}
+
+
+
+//display header section of department
+
+function displayDeptFirstSection(deptName){
+  document.querySelector('.dept-first-sec').innerHTML = `
+  <h1>${deptName} Department</h1>
+    <div class="dept-sub-container">
+      <input class="search-bar" type="text" placeholder="Search">
+      <div>
+        <button class="add-doctor js-add-doc" onclick="toggleAddDoctorOverlay()">Add Doctor</button>
+        <button class="delete-doctor" onclick="toggleDeleteDoctorOverlay()">Delete Doctor</button>
+      </div>
+    </div>
+  `
+}
+
+
+// document.querySelector('.js-add-doc').addEventListener('click',()=>{
+//   toggleOverlay();
+// })
+
+//overlay function
+
+
+function toggleAddDoctorOverlay() {
+  let element = document.querySelector('.overlay');
+    if(element.style.visibility === 'hidden'){
+      element.style.visibility = 'visible';
+    }
+    else{
+      element.style.visibility = 'hidden';
+    }
+}
+
+function toggleDeleteDoctorOverlay() {
+  let element = document.querySelector('.delete-doc-overlay');
+    if(element.style.visibility === 'hidden'){
+      element.style.visibility = 'visible';
+    }
+    else{
+      element.style.visibility = 'hidden';
+    }
+}
+
+ //overlay add doctor inner content
+document.querySelector('.add-doc-overlay').innerHTML = 
+`<div class="add-doc-dropdown-box">
+<p class="add-title">ADD DOCTOR</p>
+<p>Full Name:</p>
+<input type="text" class="doc-name">
+<p>Age:</p>
+<input type="text" class="doc-age">
+<p>Contact No.:</p>
+<input type="tel" class="doc-contact">
+<p>Email id:</p>
+<input type="email" class="doc-email">
+<p>Gender:</p>
+<select name="doc-sex" id="" class="doc-sex">
+  <option value="">Select</option>
+  <option value="Male">Male</option>
+  <option value="Female">Female</option>
+</select>
+<p>Address:</p>
+<textarea class="doc-address" name="" id="" cols="30" rows="4"></textarea>
+<div class="btn-ctn">
+  <button class="add-doc-btn js-add-doc-btn">Submit</button>
+</div>
+</div>`
+
+//overlay delete doctor inner content 
+document.querySelector('.delete-doc-overlay').innerHTML = `
+<div class="delete-doc-dropdown-box">
+<p class="add-title">DELETE DOCTOR</p>
+<p>Enter Doctor Id:</p>
+<input type="text" class="doc-id">
+<div class="btn-ctn">
+<button class="delete-doc-btn">Delete Doctor</button>
+</div>
+<p class="invalid-doctor"></p>
+</div>
+`
+
+
+//Add doctor
+
+function addDoctor(deptId,dept,n){
+  document.querySelector('.js-add-doc-btn').addEventListener('click',()=>{
+    let Name = document.querySelector('.doc-name').value;
+    let Age = document.querySelector('.doc-age').value;
+    let Phone = document.querySelector('.doc-contact').value;
+    let sexIndex = document.querySelector('.doc-sex').selectedIndex;
+    let Sex = document.querySelector('.doc-sex')[sexIndex].value;
+  
+    let newDoc = new createDoctor(++n,Name,deptId,Age,Phone,Sex);
+    console.log(newDoc);
+    dept.push(newDoc);
+    displayDoctorList(dept);
+    toggleAddDoctorOverlay();
+  })
+}
+
+//create doctor
+function createDoctor(doctorId,Name,deptId,Age,Phone,Sex){
+  this.doctorId = doctorId;
+  this.Name = Name;
+  this.deptId = deptId
+  this.Age = Age;
+  this.Phone = Phone;
+  this.Sex = Sex;
+}
+
+//delete doctor
+function deleteDoctor(dept){
+  let c = 0;
+  document.querySelector('.delete-doc-btn').addEventListener('click',()=>{
+    let docId = document.querySelector('.doc-id').value;
+    dept.forEach(doctor => {
+      if(doctor.doctorId == docId){
+        dept.splice((docId-1),1);
+        c++;
+      }
+    });
+    if(c >0){
+    displayDoctorList(dept);
+    toggleDeleteDoctorOverlay();
+    document.querySelector('.invalid-doctor').innerHTML = "";
+    }
+    else{
+      document.querySelector('.invalid-doctor').innerHTML = `Invalid Doctor Id`;
+    }
+  })
+
+}
+
+//display doctor list
+
+function displayDoctorList(department) {
+  const doctors = document.querySelector('.department-table');
+  doctors.innerHTML = `
+  <tr>
+    <th>Doctor Id</th>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Phone</th>
+    <th>Sex</th>
+  </tr>`
+
+  let j = 0;
+  department.forEach(data => {
+  
+    doctors.innerHTML += `
+    <tr>
+    <td>${data.doctorId}</td>
+    <td>${data.Name}</td>
+    <td>${data.Age}</td>
+    <td>${data.Phone}</td>
+    <td>${data.Sex}</td>
+    </tr>
+    `
+    j++;
+    
+  });
+  return j;
+}
